10842e04d65f875749bf0606d7789d3b45e35110
victoriafdelgado <vickyfdelgado@gmail.com>
2026-09-10 17:14:03 -0300

    paso constantes en go a mayuscula

--- services/client/src/protocol/protocol.go
+++ services/client/src/protocol/protocol.go
@@ -10,27 +10,25 @@ import (
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/safe_socket"
 )
 
-const (
-	ReceiveMessage byte = iota
-	DoneSendingBets
-	SendBatch
-	BatchACK
-	AnnounceAgency
-)
+const RECIEVE_RESULT_MESSAGE = 0
+const SEND_DONE = 1
+const SEND_BATCH = 2
+const RECEIVE_BATCH_ACK = 3
+const SEND_AGENCY_ANNOUNCEMENT = 4
 
-const Delimiter = ','
-const FixedHeaderSize = 5
+const DELIMITER = ','
+const FIXED_HEADER_SIZE = 5
 
 func serializeBet(bet domain.Bet) []byte {
 	var buf []byte
 	buf = append(buf, []byte(bet.Name)...)
-	buf = append(buf, Delimiter)
+	buf = append(buf, DELIMITER)
 	buf = append(buf, []byte(bet.Surname)...)
-	buf = append(buf, Delimiter)
+	buf = append(buf, DELIMITER)
 	buf = append(buf, []byte(strconv.Itoa(bet.Id))...)
-	buf = append(buf, Delimiter)
+	buf = append(buf, DELIMITER)
 	buf = append(buf, []byte(bet.DateOfBirth)...)
-	buf = append(buf, Delimiter)
+	buf = append(buf, DELIMITER)
 	buf = append(buf, []byte(strconv.Itoa(bet.Number))...)
 	return buf
 }
@@ -48,8 +46,8 @@ func serializeBets(bets []domain.Bet) []byte {
 
 func SendAnnounceAgencyMessage(agencyId string, socket io.Writer) error {
 	payload := []byte(agencyId)
-	header := make([]byte, FixedHeaderSize)
-	header[0] = AnnounceAgency
+	header := make([]byte, FIXED_HEADER_SIZE)
+	header[0] = SEND_AGENCY_ANNOUNCEMENT
 	binary.BigEndian.PutUint32(header[1:], uint32(len(payload)))
 	message := append(header, payload...)
 	return safe_socket.SendAll(socket, message)
@@ -57,26 +55,26 @@ func SendAnnounceAgencyMessage(agencyId string, socket io.Writer) error {
 
 func SendBatchMessage(bets []domain.Bet, socket io.Writer) error {
 	payload := serializeBets(bets)
-	header := make([]byte, FixedHeaderSize)
-	header[0] = SendBatch
+	header := make([]byte, FIXED_HEADER_SIZE)
+	header[0] = SEND_BATCH
 	binary.BigEndian.PutUint32(header[1:], uint32(len(payload)))
 	message := append(header, payload...)
 	return safe_socket.SendAll(socket, message)
 }
 
 func SendDoneMessage(socket io.Writer) error {
-	header := make([]byte, FixedHeaderSize)
-	header[0] = DoneSendingBets
+	header := make([]byte, FIXED_HEADER_SIZE)
+	header[0] = SEND_DONE
 	binary.BigEndian.PutUint32(header[1:], 0)
 	return safe_socket.SendAll(socket, header)
 }
 
 func ReceiveResultMessage(socket io.Reader) (string, error) {
-	header, err := safe_socket.RecvAll(socket, FixedHeaderSize)
+	header, err := safe_socket.RecvAll(socket, FIXED_HEADER_SIZE)
 	if err != nil {
 		return "", err
 	}
-	if header[0] != ReceiveMessage {
+	if header[0] != RECIEVE_RESULT_MESSAGE {
 		return "", fmt.Errorf("Codigo invalido")
 	}
 	length := binary.BigEndian.Uint32(header[1:])
@@ -88,11 +86,11 @@ func ReceiveResultMessage(socket io.Reader) (string, error) {
 }
 
 func ReceiveBatchACK(socket io.Reader) (string, error) {
-	header, err := safe_socket.RecvAll(socket, FixedHeaderSize)
+	header, err := safe_socket.RecvAll(socket, FIXED_HEADER_SIZE)
 	if err != nil {
 		return "", err
 	}
-	if header[0] != BatchACK {
+	if header[0] != RECEIVE_BATCH_ACK {
 		return "", fmt.Errorf("Codigo invalido")
 	}
 	length := binary.BigEndian.Uint32(header[1:])
